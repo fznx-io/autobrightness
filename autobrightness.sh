914251b35bf747f27b927540eeaef879b33b2fd4
Fauzan <mufahaza@gmail.com>
2016-08-26 14:20:00 +0700

    Add configurable brightness variables

--- autobrightness.sh
+++ autobrightness.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Configuration
+NIGHT_BRIGHTNESS=0
+DAY_BRIGHTNESS=3
+
 if [ `date +%H` -lt 7  -o `date +%H` -gt 17 ]; then
-echo 0 > /sys/class/backlight/acpi_video0/brightness
+echo $NIGHT_BRIGHTNESS > /sys/class/backlight/acpi_video0/brightness
 else
-echo 3 > /sys/class/backlight/acpi_video0/brightness
+echo $DAY_BRIGHTNESS > /sys/class/backlight/acpi_video0/brightness
 fi
